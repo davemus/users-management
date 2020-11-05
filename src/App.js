246d0df520e9a07e5f57951b118de3a28f51e8bf
davemus <drozdovud@gmail.com>
2020-11-05 09:28:31 +0200

    Created table component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+
+
+function TableHeaderRow(props) {
+   return (
+    <tr>
+      {props.data.map((cell) => <th>{cell}</th>)}
+    </tr>
+  ) 
+}
+
+function TableRow(props) {
+  return (
+    <tr>
+      {props.data.map((cell) => <td>{cell}</td>)}
+    </tr>
+  )
+}
+
+function Table(props) {  
+  return (
+    <table>
+      <TableHeaderRow data={props.headerRow}/>
+      {props.rows.map((row) => <TableRow data={row}/>)}
+    </table>
+  )
+}
+
+const headerRow = ['ID', 'Email', 'Username', 'First Name', 'Last Name'];
+const rows = [
+  [1, 'sonya.hohlovskaia@gmail.com', 'supersofa', 'Sonia', 'Hohlovskaia'],
+  [2, 'viktoria.simpleyarova@gmail.com', 'vivi', 'Viktoria', 'Simpleyarova'],
+  [3, 'dmitriy.gordovitiy@yandex.com', 'dimon', 'Dmitriy', 'Gordovitiy'],
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Table headerRow={headerRow} rows={rows}/>
+  )
 }
 
 export default App;
