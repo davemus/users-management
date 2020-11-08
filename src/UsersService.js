7145675406d16ca6eedf66706974e3b27eec601f
davemus <drozdovud@gmail.com>
2020-11-08 09:53:28 +0200

    Create service, that will handle users retrieving

---
+++ src/UsersService.js
@@ -0,0 +1,38 @@
+class UsersService {
+    static baseEndpoint = 'http://localhost:3001/users'; 
+
+    static commonRequestOptions = { 
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    }
+
+    static makeRequest(method, id=undefined, data=undefined) {
+        const endpoint = id ? this.baseEndpoint + '/' + id : this.baseEndpoint;
+        const requestOptions = Object.assign(
+            {'method': method, 'data': data},
+            this.commonRequestOptions
+        );
+        return fetch(endpoint, requestOptions).then(
+            (response) => response.json(),
+        );
+    }
+
+    static detail(id) {
+        return this.makeRequest('GET', id);
+    }
+
+    static create(data) {
+        return this.makeRequest('POST', undefined, data);
+    }
+
+    static update(id, data) {
+        return this.makeRequest('PUT', id, data);
+    }
+
+    static list() {
+        return this.makeRequest('GET');
+    }
+}
+
+export default UsersService;
