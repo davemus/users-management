bd261b941b2228e768c0efa9c4b0451d264eba9c
davemus <drozdovud@gmail.com>
2020-11-12 06:23:57 +0200

    Add js docs

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,19 @@
+/**
+ * @param {Array} arr1
+ * @param {Array} arr2 array with same length, as arr1
+ * @return {Array} new Array, which elements are Arrays, that stores pairs of
+ *     elements with same index from arr1 and arr2
+ */
 export function zip(arr1, arr2) {
   return arr1.map((k, i) => [k, arr2[i]]);
 }
 
+/**
+ * Simple and dirty copy. It doesn't work with: Date, undefined, function,
+ *     NaN, RegExp and complex objects.
+ * @param {Object} obj
+ * @return {Object} copy of obj
+ */
 export function copy(obj) {
   return JSON.parse(JSON.stringify(obj));
 }
